1602542206ebd7aabf02a7e08bd129a3dacc4138
simon4100 <159449461+simon4100@users.noreply.github.com>
2025-03-27 12:01:56 +0100

    Simon Scene + Playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,87 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    private CharacterController controller;
+    [SerializeField] private new Transform camera;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float turningSpeed = 2f;
+    [SerializeField] private float gravity = 6f;
+    [SerializeField] private float jumpForce = 1f;
+
+    [SerializeField] private float verticalVelocity;
+
+    [Header("Input")]
+    private float moveInput;
+    private float turnInput;
+    private float mouseX;
+    private float mouseY;
+    private float verticalRotation = 0f;
+
+    private void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        InputManagement();
+        Movement();
+    }
+
+    private void Movement()
+    {
+        GroundMovement();
+        Turn();
+    }
+    
+    private void GroundMovement()
+    {
+        Vector3 move = new Vector3 (turnInput, 0, moveInput);
+        move = transform.TransformDirection(move);
+
+        move.y = VerticalForceCalculation();
+
+        move *= walkSpeed;
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private void Turn()
+    {
+        transform.Rotate(Vector3.up * mouseX * turningSpeed);
+        verticalRotation -= mouseY * turningSpeed;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f); // Prevent flipping
+
+        camera.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = Mathf.Sqrt(jumpForce * gravity * 2);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        return verticalVelocity;
+    }
+    private void InputManagement()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
     }
 }
