5f3c7d6d360742e39e113175f132482b1be676cb
shamblez-44 <engslevbenjamin@gmail.com>
2025-04-24 09:44:45 +0200

    HealthBar + StaminaBar

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -3,28 +3,11 @@ using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
-    //[SerializeField] private Health _health;
+    [SerializeField] private Health _health;
+    [SerializeField] private Slider healthBar;
 
-    [SerializeField] private RectTransform _barRect;
-
-    [SerializeField] private RectMask2D _mask;
-
-    private float _maxRightMask;
-    private float _intialRightMask;
-
-    private void Start()
+    private void Update()
     {
-        //x = left , w = top , y = bottom, z = right
-        _maxRightMask = _barRect.rect.width - _mask.padding.x - _mask.padding.z;
-        _intialRightMask = _mask.padding.z;
-    }
-
-    public void SetValue(int newValue)
-    {
-        var targetWidth = newValue * _maxRightMask / 100; //_health.MaxHp;
-        var newRightMask = _maxRightMask + _intialRightMask - targetWidth;
-        var padding = _mask.padding;
-        padding.z = newRightMask;
-        _mask.padding = padding;
+        healthBar.value = _health.CurrentHealth / _health.maxHealth;
     }
 }
