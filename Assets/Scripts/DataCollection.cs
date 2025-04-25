9d35d4cfb28e53aaebce7329b4f4d977754f6fc0
shamblez-44 <engslevbenjamin@gmail.com>
2025-04-25 12:41:07 +0200

    Minor health change + datacollection

--- Assets/Scripts/DataCollection.cs
+++ Assets/Scripts/DataCollection.cs
@@ -1,10 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class DataCollection : MonoBehaviour
 {
-    [SerializeField] public static int deaths = 0;
-    [SerializeField] public static float timeBeforePlay = 0;
-    private bool playing = false;
+    public static int deaths = 0;
+    public static float timeBeforePlay = 0;
+    public float timeToInsideOuterWalls = 0;
+    public float timeToCastle = 0;
+    [SerializeField] private OuterWall outerWall;
+    [SerializeField] private Castle innerWall;
+    public TextMeshProUGUI deathsText;
+    public TextMeshProUGUI timeToWallsText;
+    public TextMeshProUGUI timeToCastleText;
 
     public void Update()
     {
@@ -13,20 +20,21 @@ public class DataCollection : MonoBehaviour
             Restart();
         }
 
-        if(playing == false)
+        if(outerWall.insideTheOuterWall == false)
         {
-            timeBeforePlay += Time.deltaTime;
+            timeToInsideOuterWalls += Time.deltaTime;
+        }
+
+        if(innerWall.insideCastle == false)
+        {
+            timeToCastle += Time.deltaTime;
         }
     }
 
     public void Died()
     {
         deaths++;
-    }
-
-    public void StartedPlay()
-    {
-        playing = true;
+        deathsText.text = deaths.ToString();
     }
 
     public void Restart()
