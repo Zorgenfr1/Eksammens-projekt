7b1a994aa6db86d6dd9c9f9f5faf555e71d609d5
Supermikdkk <159448928+Supermikdkk@users.noreply.github.com>
2025-04-28 10:58:31 +0200

    Attack Logic

--- Assets/SwordHitPlayer.cs
+++ Assets/SwordHitPlayer.cs
@@ -7,6 +7,7 @@ public class SwordHitPlayer : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
+            Debug.LogError("Sword hit enemy");
             EnemyHealth enemy = other.GetComponent<EnemyHealth>();
             enemy.ChangeHealth(-lightAttack.damage);
         }
