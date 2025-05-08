51e57675cb94ca56305eed7d1d75c82c437e7245
Supermikdkk <159448928+Supermikdkk@users.noreply.github.com>
2025-05-08 11:03:55 +0200

    Clang sound effect

--- Assets/Scripts/SwordHitPlayer.cs
+++ Assets/Scripts/SwordHitPlayer.cs
@@ -6,7 +6,12 @@ public class SwordHitPlayer : MonoBehaviour
     public Attacks lightAttack;
     public GameObject mainCamera;
     private Vector3 spawnPosition;
+    private AudioSource swordAudio;
 
+    private void Start()
+    {
+        swordAudio = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
@@ -20,6 +25,7 @@ public class SwordHitPlayer : MonoBehaviour
             }
             EnemyHealth enemy = other.GetComponent<EnemyHealth>();
             enemy.ChangeHealth(-lightAttack.damage);
+            swordAudio.PlayOneShot(lightAttack.hitSound);
             Instantiate(lightAttack.bloodEffect, spawnPosition, Quaternion.identity, other.transform);
             Debug.Log(spawnPosition);
 
